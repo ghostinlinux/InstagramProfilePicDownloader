fcaf7e4d6af90d74cb788ef0d27cc6728e3140c2
Pratik Singh <67475692+ghostinlinux@users.noreply.github.com>
2021-06-26 01:50:56 +0530

    Update instadpdownloader.py

--- instadpdownloader.py
+++ instadpdownloader.py
@@ -6,8 +6,7 @@ def instadpdownloader(*args):
     for user in args:
         instance = instaloader.Instaloader()
         os.chdir(os.path.join(os.path.expanduser("~"), device_location))
-        user_folder = instance.download_profile(user, profile_pic_only=True)
-    return user_folder
+        instance.download_profile(user, profile_pic_only=True)
 
 
 device_location = input("Enter device location : ")
